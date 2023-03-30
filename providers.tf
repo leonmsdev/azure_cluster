5d5ebe40ee662a7f027438c2666c67bdd7c4686e
leonmsdev <leonms.dev@gmail.com>
2023-03-30 21:39:54 +0200

    feat(ci): add new cicd pipeline workflow

--- providers.tf
+++ providers.tf
@@ -4,7 +4,7 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "~>3.0"
+      version = ">= 3.7.0"
     }
     random = {
       source  = "hashicorp/random"
@@ -20,9 +20,10 @@ terraform {
       storage_account_name = "tfstatedefault"
       container_name       = "tfstate"
       key                  = "terraform.tfstate"
+      use_oidc             = true
   }
 }
 
 provider "azurerm" {
-  features {}
+  use_oidc = true
 }
